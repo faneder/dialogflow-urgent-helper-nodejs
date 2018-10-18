a7eb0c21b12a4056546de4cfaa74f69079ea848b
faneder <fanedercode@gmail.com>
2018-10-18 16:09:49 +0800

    refactoring

--- functions/index.js
+++ functions/index.js
@@ -4,10 +4,10 @@ const {
     dialogflow,
     Permission,
 } = require('actions-on-google');
-const { responses } = require('./responses');
+const {responses} = require('./responses');
 
 // Instantiate the Dialogflow Client.
-const app = dialogflow({ debug: true });
+const app = dialogflow({debug: true});
 
 // Import the firebase-functions package for deployment.
 const functions = require('firebase-functions');
@@ -16,13 +16,13 @@ const config = functions.config();
 const maps = require('@google/maps');
 const googleMapsClient = maps.createClient({
     key: config.maps.key,
-    Promise: Promise
+    Promise: Promise,
 });
 
 // audio config
 const sounds = {
     alarmClock: 'https://actions.google.com/sounds/v1/alarms/alarm_clock.ogg'
-}
+};
 
 /**
  * ask a user for permission
@@ -30,73 +30,56 @@ const sounds = {
  */
 const askPermission = (conv, options) => {
     conv.ask(new Permission(options));
-}
+};
 
 /**
  * call the audio from responses
- * @param {string} text_to_speech
+ * @param {string} textToSpeech
  */
-const askAudio = (conv, text_to_speech) => {
-    conv.ask(text_to_speech)
-}
+const askAudio = (conv, textToSpeech) => {
+    conv.ask(textToSpeech);
+};
 
 /**
  * Gets the closet places from user's coordinates.
- * @param {object} location {latitude, longitude}
- * @param {string} name
- * @param {string} hospital
- * @param {string} language
- * @param {string} rankby
+ * @param {object} params
  * @return {promise}
  * @return {results<object>}
  */
 const getPlacesNearby = async (params) => {
-    const { results, status } = await googleMapsClient.placesNearby(params).asPromise()
-    .then((response) => {
-        return response.json;
-    })
-    .catch((err) => {
-        throw new Error(`placesNearby Fetch Failed: ${err}`);
-    });
+    try {
+        const response = await googleMapsClient.placesNearby(params).asPromise()
+        const { results, status } = await response.json;
 
-    if (status === 'OK') {
-        return results[0];
+        if (status === 'OK') {
+            return results[0];
+        }
+
+        throw new Error(`getPlacesNearby Failed for the following reason: ${status}`);
+    } catch (error) {
+        throw new Error(`placesNearby Fetch Failed: ${error}`);
     }
-
-    throw new Error(`getPlacesNearby Failed for the following reason: ${status}`);
-}
+};
 
 /**
  * Gets the distance from origin address to destination addresses
- * @param {object} location {latitude, longitude}
- * @param {string} destinations
- * @param {number} departure_time
- * @param {string} mode
- * @param {array} avoid
- * @param {string} traffic_model
+ * @param {object} params
  * @return {results<object>}
  */
-
 const getDistanceMatrix = async (params) => {
-    const {
-        rows,
-        status,
-        origin_addresses,
-        destination_addresses
-    } = await googleMapsClient.distanceMatrix(params).asPromise()
-    .then((response) => {
-        return response.json;
-    })
-    .catch((err) => {
-        throw new Error(`getDistanceMatrix Fetch Failed: ${err}`);
-    });
+    try {
+        const response = await googleMapsClient.distanceMatrix(params).asPromise()
+        const {status, ...results} = await response.json;
 
-    if (status === 'OK') {
-        return { rows, origin_addresses, destination_addresses };
+        if (status === 'OK') {
+            return {...results};
+        }
+
+        throw new Error(`distanceMatrix Failed for the following reason: ${status}`);
+    } catch (error) {
+        throw new Error(`getDistanceMatrix Fetch Failed: ${error}`);
     }
-
-    throw new Error(`distanceMatrix Failed for the following reason: ${status}`);
-}
+};
 
 /**
  * Handle the Dialogflow intent named 'Default Welcome Intent'.
@@ -107,34 +90,31 @@ app.intent('Default Welcome Intent', (conv) => {
         // Ask for more than one permission. User can authorize all or none.
         permissions: ['NAME', 'DEVICE_PRECISE_LOCATION'],
     };
-    askPermission(conv, options)
+    askPermission(conv, options);
 });
 
 /**
  * Handle the Dialogflow intent named 'actions_intent_PERMISSION
  * @param {boolean} permissionGranted
  */
-// app.intent('actions_intent_PERMISSION', (conv, params, permissionGranted) => {
 app.intent('actions_intent_PERMISSION', async (conv, params, permissionGranted) => {
     if (!permissionGranted) {
         throw new Error('Permission not granted');
     }
 
-    const { coordinates } = conv.device.location;
+    const {coordinates} = conv.device.location;
     const inOneHour = Math.round((new Date().getTime() + 60 * 60 * 1000) / 1000);
 
     // find the close place
     const {
         name: hospitalName,
-        geometry: geometry
+        geometry,
      } = await getPlacesNearby({
         type: 'hospital',
         location: coordinates,
         rankby: 'distance',
         name: 'hospital',
-        opennow: true
-    }).catch((err) => {
-        throw new Error(err);
+        opennow: true,
     });
 
     const {
@@ -142,14 +122,12 @@ app.intent('actions_intent_PERMISSION', async (conv, params, permissionGranted) 
         origin_addresses,
         destination_addresses,
     } = await getDistanceMatrix({
-        origins: { lat: coordinates.latitude, lng: coordinates.longitude },
+        origins: {lat: coordinates.latitude, lng: coordinates.longitude},
         destinations: geometry.location,
         departure_time: inOneHour,
         mode: 'driving',
         avoid: ['tolls', 'ferries'],
-        traffic_model: 'best_guess'
-    }).catch((err) => {
-        throw new Error(err);
+        traffic_model: 'best_guess',
     });
 
     const emergencyInfo = {
@@ -159,8 +137,8 @@ app.intent('actions_intent_PERMISSION', async (conv, params, permissionGranted) 
         userName: conv.user.name.display,
         userAddress: origin_addresses[0],
         coordinates: JSON.stringify(coordinates),
-        durationTraffic: rows[0].elements[0].duration_in_traffic.text
-    }
+        durationTraffic: rows[0].elements[0].duration_in_traffic.text,
+    };
 
     askAudio(conv, responses.emergencyNotify(emergencyInfo));
 });
